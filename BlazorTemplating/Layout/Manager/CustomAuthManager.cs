dc0233125e1854a8ae678fba3ec354442227bb8c
praweshkafle <75798623+Praweshkafle@users.noreply.github.com>
2024-02-23 22:08:22 +0545

    updated

---
+++ BlazorTemplating/Layout/Manager/CustomAuthManager.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorTemplating.Layout.Manager
+{
+    public class CustomAuthManager : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthManager(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var key = await _localStorageService.GetItemAsync<string>("Authorized");
+
+            if (string.IsNullOrEmpty(key))
+            {
+                return new AuthenticationState(new System.Security.Claims.ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(key)));
+        }
+
+        public void NotifyAuthState()
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
--- BlazorTemplating/Program.cs
+++ BlazorTemplating/Program.cs
@@ -1,4 +1,8 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
 using BlazorTemplating;
+using BlazorTemplating.Layout.Manager;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,5 +10,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredToast();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthManager>();
 
+builder.Services.AddAuthorizationCore();
 await builder.Build().RunAsync();
